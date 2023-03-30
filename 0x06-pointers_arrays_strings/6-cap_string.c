6ad2924f541cd4e0ad637ff4c83629c030698ab1
Salma Hussien <salmaa.hussien1@gmail.com>
2023-03-29 22:58:38 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,31 +6,26 @@
  */
 char *cap_string(char *str)
 {
-	int index = 0;
+	int i = 0;
 
-	while (str[index])
+	while (str[i])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] -= 32;
-
-		index++;
+		if (str[i] == ' ' ||
+		    str[i] == '\t' ||
+		    str[i] == '\n' ||
+		    str[i] == ',' ||
+		    str[i] == ';' ||
+		    str[i] == '.' ||
+		    str[i] == '!' ||
+		    str[i] == '?' ||
+		    str[i] == '"' ||
+		    str[i] == '(' ||
+		    str[i] == ')' ||
+		    str[i] == '{' ||
+		    str[i] == '}')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] -= 32;
+		i++;
 	}
-
 	return (str);
 }
