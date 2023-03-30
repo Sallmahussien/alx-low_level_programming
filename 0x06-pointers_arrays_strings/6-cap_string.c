d4f4bd23e98840df3e573c01bf70fd24ac340e0b
Salma Hussien <salmaa.hussien1@gmail.com>
2023-03-29 23:07:24 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -25,8 +25,6 @@ char *cap_string(char *str)
 		    str[i] == '}')
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
-		if (str[i] == '\t')
-			str[i] = ' ';
 		i++;
 	}
 	return (str);
