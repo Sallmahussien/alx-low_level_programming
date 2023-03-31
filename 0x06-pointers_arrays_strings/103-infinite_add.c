b01381c5e1f0d048491dc3541864cd5b028474fc
Salma Hussien <salmaa.hussien1@gmail.com>
2023-03-30 17:07:44 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -16,34 +16,26 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		size_n1++;
 	while (n2[size_n2] != '\0')
 		size_n2++;
-	if (size_n1 > size_n2)
-		len = size_n1;
-	else
-		len = size_n2;
-	if (len + 1 >= size_r)
+	len = (size_n1 > size_n2) ? size_n1 : size_n2;
+	if (len + 1 > size_r)
 		return (0);
 	for (i = len - 1; i >= 0; i--)
 	{
 		size_n1--;
 		size_n2--;
-		if (size_n1 >= 0)
-			h = n1[size_n1] - '0';
-		else
-			h = 0;
-		if (size_n2 >= 0)
-			k = n2[size_n2] - '0';
-		else
-			k = 0;
+		h = (size_n1 >= 0) ? n1[size_n1] - '0' : 0;
+		k = (size_n2 >= 0) ? n2[size_n2] - '0' : 0;
 		sum = h + k + rem;
-		if (sum >= 10)
-			rem = sum / 10;
-		else
-			rem = 0;
+		rem = (sum >= 10) ? sum / 10 : 0;
 		r[i + 1] = (sum % 10) + '0';
 	}
 	r[0] = rem + '0';
 	if (rem == 1)
+	{
 		r[len + 1] = '\0';
+		if (len + 2 > size_r)
+			return (0);
+	}
 	else
 		r[len] = '\0';
 	return (r);
