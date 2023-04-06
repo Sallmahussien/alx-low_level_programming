fd3442a4ee826157cb9e1fb376c6695896b1bc36
Salma Hussien <salmaa.hussien1@gmail.com>
2023-04-05 17:56:35 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
+int _strlen_recursion(char *s);
 int _plaindrome(char *s, int len, int index);
+
+/*
+ *  _strlen_recursion -  returns the length of a string.
+ *  @s: string.
+ *  Return: string length.
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
+}
 
 /**
  * _plaindrome - checks plaindrome string.
