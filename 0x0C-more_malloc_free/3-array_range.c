59502852b43b4d8b46518bbcf3f1918f087b6526
Salma Hussien <salmaa.hussien1@gmail.com>
2023-04-13 15:45:00 -0700

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,12 +12,12 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	size  = max - min + 1;
+	size = max - min + 1;
 	temp = malloc(sizeof(int) * size);
 	if (temp == NULL)
 		return (NULL);
 
-	for (i = min; i <= max; i++)
+	for (i = 0; i <= size; i++)
 		temp[i] = min++;
 	return (temp);
 }
