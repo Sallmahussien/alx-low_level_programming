d2482711bd53f6036c9c4e0e11c5aa55ff7e17c3
Salma Hussien <salmaa.hussien1@gmail.com>
2023-04-13 15:21:37 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	temp = arr;
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		temp[i] = '\0';
 	return (temp);
 }
