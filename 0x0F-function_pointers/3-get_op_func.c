74011ee2aee51fb789ab740aaab5153c48e6bc22
Salma Hussien <salmaa.hussien1@gmail.com>
2023-04-19 14:32:07 -0700

    update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,9 +18,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 5)
+	while (ops[i].op)
 	{
-		if (s && s[0] == ops[i].op[0] && !s[1])
+		if (*(ops[i].op) == *s)
 			return (ops[i].f);
 		i++;
 	}
