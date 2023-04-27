3dc9e44af93c4489674175308681791f4536de54
salma hussien <salmaa.hussien1@gmail.com>
2023-04-27 12:33:10 -0700

    print list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct list - linked list
+ * @str: string
+ * @len: string length
+ * @next: pointer to next node
+ */
+typedef struct list
+{
+	char *str;
+	unsigned int len;
+	struct list *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
