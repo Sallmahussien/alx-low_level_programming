fede6261d2bcc42d4fa92784310bb361892c613e
salma hussien <salmaa.hussien1@gmail.com>
2023-04-29 15:27:51 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current_node;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		current_node = *head;
 		*head = current_node->next;
 		free(current_node);
 	}
+	head = NULL;
 }
