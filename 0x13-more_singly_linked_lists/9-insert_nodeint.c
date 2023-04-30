7c3f5757c2fb53d403f94197fe3144d1d0a19829
salma hussien <salmaa.hussien1@gmail.com>
2023-04-29 19:34:51 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	while (tmp)
 	{
+		if (tmp == NULL || tmp->next == NULL)
+			return (NULL);
 		if (idx == 0)
 		{
 			new_node->next = *head;
