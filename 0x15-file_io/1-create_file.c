276e152330dcda7f345e51d5b057c0569d1327c2
root <root@DESKTOP-UKMLNQD.localdomain>
2023-05-08 22:39:11 +0300

    append to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 
-	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (op == -1)
 		return (-1);
 
