d28bb05fd3f8ddf7b4176452dba5d96a2651bd9b
root <root@DESKTOP-UKMLNQD.localdomain>
2023-05-08 21:41:31 +0300

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int op, wr, len = 0; 
+	int op, wr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -17,14 +17,17 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 
-	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (op == -1)
 		return (-1);
 
 	wr = write(op, text_content, len);
-	
+
 	if (wr == -1)
+	{
+		close(op);
 		return (-1);
+	}
 
 	close(op);
 	return (1);
