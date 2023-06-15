62293fb01311ad2f0978acbe3576339d8215bd0f
salma hussien <salmaa.hussien1@gmail.com>
2023-06-15 23:20:11 +0300

    Edit Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	for (i = 0; i < idx - 1 && tmp; i++)
 		tmp = tmp->next;
 
+	if (!tmp)
+		return (NULL);
+
 	new_node->prev = tmp;
 	new_node->next = tmp->next;
 	tmp->next->prev = new_node;
