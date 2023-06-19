fdeb55a5923021c5f6903fad4b8a1bf44283eda7
salma hussien <salmaa.hussien1@gmail>
2023-06-19 20:01:56 +0300

    Task 2

---
+++ 0x18-dynamic_libraries/2-calculator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
+int sub(int num1, int num2)
+{
+	return (num1 - num2);
+}
+
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+int div(int num1, int num2)
+{
+	if (num2 == 0)
+		return (0);
+	return (num1 / num2);
+}
+
+int mod(int num1, int num2)
+{
+	if (num2 == 0)
+		return (0);
+	return (num1 % num2);
+}
