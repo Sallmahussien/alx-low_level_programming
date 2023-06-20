e7fb576c2c511b413ec19cd6471e1e20c9d76110
salma hussien <salmaa.hussien1@gmail>
2023-06-20 05:52:58 +0300

    Edit task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+wget -P .. https://github.com/Sallmahussien/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libwinning.so
 export LD_PRELOAD = /root/alx-low_level_programming/0x18-dynamic_libraries/libwinning.so
