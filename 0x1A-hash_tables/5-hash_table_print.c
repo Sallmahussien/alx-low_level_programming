fc23a6d87cd41ece56f4d3c61df395124a9321fc
salma hussien <salmaa.hussien1@gmail.com>
2023-07-07 03:48:56 +0300

    Add task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: is the hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long idx;
+	hash_node_t *current_node;
+	int flag = 0;
+
+	if (!ht)
+		return;
+
+	printf("{");
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		current_node = ht->array[idx];
+		while (current_node)
+		{
+			if (flag == 1)
+			{
+				printf(", ");
+			}
+
+			printf("'%s': '%s'", current_node->key, current_node->value);
+			current_node = current_node->next;
+			flag = 1;
+		}
+		
+	}
+	printf("}\n");
+}
